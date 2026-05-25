9d0336b97127313c5b38488c8241f10baf6f6719
Maryam <masumaakhtermaryam@gmail.com>
2026-05-25 13:03:29 +0600

    feat: add blackjack game

---
+++ index.js
@@ -0,0 +1,51 @@
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el") 
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 10) + 2
+    return randomNumber
+}
+
+function startGame() {
+    isAlive = true
+    hasBlackJack = false
+    cards = [getRandomCard(), getRandomCard()]
+    sum = cards[0] + cards[1]
+    renderGame()
+}
+
+function renderGame() {
+cardsEl.textContent = "Cards: " + cards[0] + "-" + cards[1]
+sumEl.textContent = "Sum: " + sum
+if (sum <= 20) {
+   message = "Do you want to draw a new card?"
+} else if (sum === 21) {
+    message="Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+} else {
+     message = "You're out of the game!"
+     isAlive = false
+}
+messageEl.textContent = message
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        startGame()
+    }
+}
+
+
+
+
+
